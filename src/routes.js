8fb9e314f443af4036292d7f1535053a2c2a7dd1
FlorSanders <florsanders@gmail.com>
2020-05-04 15:28:11 +0200

    And again

--- src/routes.js
+++ src/routes.js
@@ -15,20 +15,20 @@ const basename = process.env.PUBLIC_URL; // Run on subdirectory?
 const privateRoutes = [
   {
     id: 'iot-graph',
-    path: basename + '/iot-graph',
+    path: '/iot-graph',
     component: IoTGraph
   }
 ];
 
 const Routes = () => (
-  <Router>
+  <Router basename={basename}>
     <Fragment>
       <Switch>
         <NotLoggedInLayout component={Login} path={basename + "/login"} exact />
         <NotLoggedInLayout component={Register} path={basename + "/register"} exact />
         <NotLoggedInLayout path={basename + "/register/success"} component={RegistrationSuccess} exact />
         <PublicLayout path={basename + "/404"} component={PageNotFound} exact />
-        <Redirect from={basename + "/"} to={basename + "/iot-graph"} exact />
+        <Redirect from={basename + "/"} to={"/iot-graph"} exact />
         <PrivateLayout path={basename + "/"} routes={privateRoutes} />
         <Redirect to="/404" />
       </Switch>
