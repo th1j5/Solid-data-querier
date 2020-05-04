1f1fe7c7ebc648632e7117f55342b415d7bd1605
FlorSanders <florsanders@gmail.com>
2020-05-04 15:42:46 +0200

    hellothere

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const basename = process.env.PUBLIC_URL; // Run on subdirectory?
 const privateRoutes = [
   {
     id: 'iot-graph',
-    path: basename + '/iot-graph',
+    path: '/iot-graph',
     component: IoTGraph
   }
 ];
@@ -24,13 +24,13 @@ const Routes = () => (
   <Router basename={basename}>
     <Fragment>
       <Switch>
-        <NotLoggedInLayout component={Login} path={basename + "/login"} exact />
-        <NotLoggedInLayout component={Register} path={basename + "/register"} exact />
-        <NotLoggedInLayout path={basename + "/register/success"} component={RegistrationSuccess} exact />
+        <NotLoggedInLayout component={Login} path={ "/login"} exact />
+        <NotLoggedInLayout component={Register} path={ "/register"} exact />
+        <NotLoggedInLayout path={ "/register/success"} component={RegistrationSuccess} exact />
         <PublicLayout path={basename + "/404"} component={PageNotFound} exact />
-        <Redirect from={basename + "/"} to={basename + "/iot-graph"} exact />
-        <PrivateLayout path={basename + "/"} routes={privateRoutes} />
-        <Redirect to={basename + "/404"} />
+        <Redirect from={basename + "/"} to={ "/iot-graph"} exact />
+        <PrivateLayout path={ "/"} routes={privateRoutes} />
+        <Redirect to={ "/404"} />
       </Switch>
     </Fragment>
   </Router>
