9a782b4d880b9584ef45ca9547bebfb3a761d6aa
FlorSanders <florsanders@gmail.com>
2020-05-04 13:35:41 +0200

    Updated routes, works now?

--- src/routes.js
+++ src/routes.js
@@ -22,11 +22,11 @@ const Routes = () => (
   <Router basename={'/Solid-IoT'}>
     <Fragment>
       <Switch>
-        <NotLoggedInLayout component={Login} path="/login" exact />
-        <NotLoggedInLayout component={Register} path="/register" exact />
-        <NotLoggedInLayout path="/register/success" component={RegistrationSuccess} exact />
-        <PublicLayout path="/404" component={PageNotFound} exact />
-        <Redirect from="/" to="/iot-graph" exact />
+        <NotLoggedInLayout component={Login} path="/login"  />
+        <NotLoggedInLayout component={Register} path="/register"  />
+        <NotLoggedInLayout path="/register/success" component={RegistrationSuccess}  />
+        <PublicLayout path="/404" component={PageNotFound}  />
+        <Redirect from="/" to="/iot-graph"  />
         <PrivateLayout path="/" routes={privateRoutes} />
         <Redirect to="/404" />
       </Switch>
